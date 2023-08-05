d19137db4ee9ca5ab1238d740eed919777ed7551
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-08-05 17:49:26 +0300

    [back-end] Updated Contact Model

--- laravel-backend/app/Models/Contact.php
+++ laravel-backend/app/Models/Contact.php
@@ -1,27 +1,24 @@
 <?php
 
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
-use MatanYadaev\EloquentSpatial\SpatialBuilder;
-use MatanYadaev\EloquentSpatial\Objects\Point;
-use MatanYadaev\EloquentSpatial\Traits\HasSpatial;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Contact extends Model
 {
     use HasFactory;
-    use HasSpatial;
 
     protected $fillable = [
+        'user_id',
         'name',
         'phone_number',
         'city',
         'country',
-        'location'
+        'latitude',
+        'longtitude'
     ];
 
     protected $cast = [
-        'location' => Point::class
     ];
 }
