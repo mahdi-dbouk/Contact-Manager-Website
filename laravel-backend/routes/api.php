f00429ddd0bc76bf3a16fbc3c9b36e29a37e914e
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-08-05 17:53:40 +0300

    [back-end] Added routes to api.php

--- laravel-backend/routes/api.php
+++ laravel-backend/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ContactController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware'=>'auth:api'], function(){
+    Route::post('/create', [ContactController::class, 'createContact']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
+
+Route::group(['prefix' => '/auth'], function(){
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+});
+
+Route::get('unauthorized', [AuthController::class, 'unauthorized'])->name("unauthorized");
