25ce7a442c31636690bd05a21279e36b15bc7e1a
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-08-07 01:56:18 +0300

    Created ContactList component

---
+++ react-frontend/src/components/ContactList/index.jsx
@@ -0,0 +1,12 @@
+import ContactCard from '../ContactCard';
+import './styles.css';
+
+function ContactList(){
+    return(
+        <div>
+            <ContactCard/>
+        </div>
+    );
+}
+
+export default ContactList;
